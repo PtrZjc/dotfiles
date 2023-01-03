6859a91cbcf3bf1ab6ca76eccd5bd2bf4883d112
Piotr Zajac <ptr.zjc@gmail.com>
2023-01-03 19:33:13 +0100

    Initial commit

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+
+# Kitty
+## Install binary
+# `curl -L https://sw.kovidgoyal.net/kitty/installer.sh | sh /dev/stdin dest=~/Applications`
+
+## Link configuration
+# ln -s ~/dotfiles/kitty/kitty.conf ~/.config/kitty/kitty.conf
+
+# symlink brewfile config
+# mkdir ~/.config/brewfile | ln -s  "$(pwd)/brew/Brewfile ~/.config/brewfile/Brewfile"
