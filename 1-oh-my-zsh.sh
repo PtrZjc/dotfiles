02522cc9384202612fa962716609d7a7bed43861
Piotr Zajac <ptr.zjc@gmail.com>
2023-06-06 12:53:46 +0200

    Updates

--- 1-oh-my-zsh.sh
+++ 1-oh-my-zsh.sh
@@ -1,2 +1,4 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+echo 'Before running 2nd script, install homebrew and 4 Meslo fonts from ./font folder'
