649bc1ad108f9daf4a5196eed2483e4193c706fc
Piotr Zajac <ptr.zjc@gmail.com>
2023-10-03 12:04:56 +0200

    Fix initialization script

--- 1-oh-my-zsh.sh
+++ 1-oh-my-zsh.sh
@@ -1,4 +1,2 @@
 #!/bin/sh
 sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-
-echo "Homebrew is not installed. Please install Homebrew and rerun this script." && exit 1
