5734d35b072a9bf2e0951f62f60d1c183d0bf5a5
Piotr Zajac <ptr.zjc@gmail.com>
2023-10-18 22:54:36 +0200

    New keybindings

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,23 +1,23 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 local act = wezterm.action
 
 local config = {}
 local open_command
 
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
 -- Determine the open command based on the OS
 if wezterm.target_triple:find("darwin") then
-  open_command = "open"
+	open_command = "open"
 else
-  open_command = "xdg-open"
+	open_command = "xdg-open"
 end
 
 -- Theme config
-config.color_scheme = 'Argonaut'
-config.font = wezterm.font 'MesloLGS Nerd Font'
+config.color_scheme = "Argonaut"
+config.font = wezterm.font("MesloLGS Nerd Font")
 config.font_size = 24
 config.hide_tab_bar_if_only_one_tab = true
 
@@ -25,27 +25,31 @@ config.hide_tab_bar_if_only_one_tab = true
 config.disable_default_mouse_bindings = true
 config.bypass_mouse_reporting_modifiers = "SHIFT"
 config.mouse_bindings = {
-  {
-    event = { Up = { streak = 1, button = 'Left' } },
-    mods = 'NONE',
-    action = act.OpenLinkAtMouseCursor,
-  },
+	{
+		event = { Up = { streak = 1, button = "Left" } },
+		mods = "NONE",
+		action = act.OpenLinkAtMouseCursor,
+	},
 }
 
 -- Keybindings config
 config.keys = {
-  {key="LeftArrow", mods="CMD", action=act{SendString="\x01"}}, -- ctrl+a -> beginning of line
-  {key="RightArrow", mods="CMD", action=act{SendString="\x05"}}, -- ctrl+e -> end of line
-  {key="LeftArrow", mods="OPT", action=act{SendString="\x1bb"}}, -- alt+b -> back word, etc
-  {key="RightArrow", mods="OPT", action=act{SendString="\x1bf"}},
-  {key="LeftArrow", mods="SHIFT", action=act{SendString="\x1bb"}},
-  {key="RightArrow", mods="SHIFT", action=act{SendString="\x1bf"}},
-  {key="Backspace", mods="CMD", action=act{SendString="\x15"}}, -- ctrl+u -> delete line
-  {key="Delete", mods="OPT", action=act{SendString="\x1bd"}}, -- alt+d -> delete word before cursor
-  {key="n", mods="CMD", action=act.DisableDefaultAssignment}, -- disable new window hotkey
-  {key="t", mods="CMD", action=act.DisableDefaultAssignment}, -- disable new pane hotkey
-  {key="CapsLock", mods="NONE", action=act{SendString="\x1b"}, -- CapsLock as escape
-  },
+	{ key = "LeftArrow", mods = "CMD", action = act({ SendString = "\x01" }) }, -- ctrl+a -> beginning of line
+	{ key = "RightArrow", mods = "CMD", action = act({ SendString = "\x05" }) }, -- ctrl+e -> end of line
+	{ key = "LeftArrow", mods = "OPT", action = act({ SendString = "\x1bb" }) }, -- alt+b -> back word, etc
+	{ key = "RightArrow", mods = "OPT", action = act({ SendString = "\x1bf" }) },
+	{ key = "LeftArrow", mods = "SHIFT", action = act({ SendString = "\x1bb" }) },
+	{ key = "RightArrow", mods = "SHIFT", action = act({ SendString = "\x1bf" }) },
+	{ key = "Backspace", mods = "CMD", action = act({ SendString = "\x15" }) }, -- ctrl+u -> delete line
+	{ key = "Delete", mods = "OPT", action = act({ SendString = "\x1bd" }) }, -- alt+d -> delete word before cursor
+	{ key = "k", mods = "CMD|OPT", action = wezterm.action({ SendString = "\x0f" }) }, -- override opt+cmd+k as ctrl+o -> kill the line (C-k)
+	{ key = "n", mods = "CMD", action = act.DisableDefaultAssignment }, -- disable new window hotkey
+	{ key = "t", mods = "CMD", action = act.DisableDefaultAssignment }, -- disable new pane hotkey
+	{
+		key = "CapsLock",
+		mods = "NONE",
+		action = act({ SendString = "\x1b" }), -- CapsLock as escape
+	},
 }
 
 -- custom hyperlinks
@@ -53,14 +57,14 @@ config.keys = {
 config.hyperlink_rules = wezterm.default_hyperlink_rules()
 
 table.insert(config.hyperlink_rules, {
-  regex = [[HUBZ-(\d+)]],
-  format = 'https://jira.allegrogroup.com/browse/HUBZ-$1',
+	regex = [[HUBZ-(\d+)]],
+	format = "https://jira.allegrogroup.com/browse/HUBZ-$1",
 })
 
 table.insert(config.hyperlink_rules, {
-  regex = [=[["' ](\w[-\w]+\/[-\w\.]+)["' ]]=],
-  format = 'https://www.github.com/$1',
-  highlight = 1
+	regex = [=[["' ](\w[-\w]+\/[-\w\.]+)["' ]]=],
+	format = "https://www.github.com/$1",
+	highlight = 1,
 })
 
 return config
