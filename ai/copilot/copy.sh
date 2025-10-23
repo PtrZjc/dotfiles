9cb35e26592702f41ec665e20f3b91f14a8f8dd9
Piotr Zajac <ptr.zjc@gmail.com>
2025-10-23 10:30:25 +0200

    Updates

--- ai/copilot/copy.sh
+++ ai/copilot/copy.sh
@@ -4,4 +4,5 @@
 rm "$HOME/.config/github-copilot/intellij/mcp.json"
 rm "$HOME/.config/github-copilot/intellij/global-copilot-instructions.md"
 ln -sf "$DOTFILES/ai/copilot/mcp.json" "$HOME/.config/github-copilot/intellij/mcp.json"
+ln -sf "$DOTFILES/ai/copilot/mcp.json" "$HOME/Library/Application Support/Code/User/mcp.json"
 ln -sf "$DOTFILES/ai/copilot/global-copilot-instructions.md" "$HOME/.config/github-copilot/intellij/global-copilot-instructions.md"
